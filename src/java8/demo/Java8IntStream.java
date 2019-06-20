28afc63ce46f7dc20841778b7a706c543eaf6175
amreshpattnaik6@gmail.com <amreshpattnaik6@gmail.com>
2019-06-21 01:31:56 +0530

    remove dublicate from list

--- src/java8/demo/Java8IntStream.java
+++ src/java8/demo/Java8IntStream.java
@@ -1,5 +1,8 @@
 package java8.demo;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Java8IntStream {
@@ -11,6 +14,14 @@ public class Java8IntStream {
 
 		// To use multi core
 		System.out.println(IntStream.range(1, 100).parallel().sum());
+
+		// removing duplicate from list
+
+		List<Integer> intList = Arrays.asList(1, 2, 3, 3, 4, 5, 6, 7, 5);
+
+		List<Integer> distintIntList = intList.stream().parallel().distinct().collect(Collectors.toList());
+
+		System.out.println(distintIntList);
 
 	}
 
